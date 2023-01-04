87c5c2a0cf79b306bf63f671a8796d4acf6c10c2
hyeonjeong lee <hlee220@myseneca.ca>
2023-01-05 00:12:51 +0900

    section 6 - 88

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import BookCreate from './components/BookCreate';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(title)
+  }
+  return <div>
+    <BookCreate onCreate={createBook} />
+  </div>
+
+}
+
+export default App
